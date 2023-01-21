57f3fa34fead9e96dfb7aaabfff3107ed0c04c88
Vahe Khalatyan <vahe99551@gmail.com>
2023-01-21 14:10:42 +0400

    i changed it  builder.Services in services field and added  Google Authentication for sign up in Google Account in program.cs

--- WorkManagementTool/Program.cs
+++ WorkManagementTool/Program.cs
@@ -6,16 +6,17 @@ using WorkManagementTool.Models.Configs;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
+var services = builder.Services;
 
 
 builder.Services.AddDbContext<WorkManagementToolContext>(options =>
     options.UseSqlServer(configuration.GetConnectionString("WorkManagementToolContext") ?? throw new InvalidOperationException("Connection string 'WorkManagementToolContext' not found.")));
 // Add services to the container.
 
-builder.Services.AddControllers();
+services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(option =>
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(option =>
 {
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     option.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
@@ -30,11 +31,18 @@ builder.Services.AddSwaggerGen(option =>
 
 //builder.Services.AddAuthorization();
 
-builder.Services.Configure<JournalConfigs>(configuration.GetSection(nameof(JournalConfigs)));
+services.Configure<JournalConfigs>(configuration.GetSection(nameof(JournalConfigs)));
+services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+});
+
 
 
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -43,6 +51,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
